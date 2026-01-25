5db8fef58d890db0cacb4adafb6cd2f5340f8607
Emaan Asif <emaan.asifsheikh2004@gmail.com>
2026-01-25 18:23:40 +0500

    Bootstrap

--- schema.sql
+++ schema.sql
@@ -1,14 +1,14 @@
--- schema.sql
+
 DROP TABLE IF EXISTS volunteers;
 
 CREATE TABLE volunteers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    phone TEXT NOT NULL,
-    age INTEGER,
-    city TEXT,
-    status TEXT,
-    institute TEXT,
-    reason TEXT
+    name TEXT NOT NULL CHECK (length(name) > 0),
+    email TEXT NOT NULL CHECK (length(email) > 0),
+    phone TEXT NOT NULL CHECK (length(phone) > 0),
+    age INTEGER NOT NULL CHECK (age > 0),
+    city TEXT NOT NULL CHECK (length(city) > 0),
+    status TEXT NOT NULL CHECK (length(status) > 0),
+    institute TEXT NOT NULL CHECK (length(institute) > 0),
+    reason TEXT NOT NULL CHECK (length(reason) > 0)
 );
